332d9d31cb638eee13fbdda45f3c5537ed220158
tigggg <105410109+tigggg@users.noreply.github.com>
2023-08-11 15:22:12 +0800

    Add files via upload
    add data models

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 class Customer:
+    def __init__(customer_id, customer_name, account_list):
+        pass
+
+class Account:
+    def __init__(account_id, transaction_list, balance):
+        pass
+
+class Transaction:
+    def __init__(transaction_id, date, amount, type, available_balance, description):
+        pass
