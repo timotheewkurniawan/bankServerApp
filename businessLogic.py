6da6c0f6c92009de734de99a035c30c84633f95b
tigggg <105410109+tigggg@users.noreply.github.com>
2023-08-11 16:05:02 +0800

    Add files via upload
    update check balance inside the withdraw and deposit money

--- businessLogic.py
+++ businessLogic.py
@@ -1,11 +1,13 @@
-from models import Account, Transaction
-
 # returns a boolean value whether it is possible to withdraw the money or not
 def withdrawMoney(account,amount):
+    balance = showBalance(account)
+    print(f'Your current balance is {balance}.')
     if account.balance < amount:
+        print('Not enough balance!')
         return False
     else:
         account.balance -= amount
+        print(f'Amount withdrawn, your current balance is {balance - amount}.')
         return True
 
 
@@ -19,9 +21,13 @@ def depositMoney(account,amount):
 
 # returns boolean value whether the transaction could proceed or not
 def transferFunds(from_account, to_account, amount):
+    from_balance = showBalance(from_account)
+    print(f'Your current balance is {balance}.')
     if from_account.balance < amount:
+        print('Not enough balance!')
         return False
     else:
         from_account.balance -= amount
+        print(f'Funds transfered, your current balance is {from_balance - amount}')
         to_account.balance += amount
         return True
